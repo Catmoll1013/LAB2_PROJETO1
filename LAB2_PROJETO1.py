86e51ea6a9ca717688a218bbed9660fad194d13c
CATARINA MOLL WESLEY <B435645@fgv.edu.br>
2024-08-12 12:42:40 -0300

    Passo 3

--- LAB2_PROJETO1.py
+++ LAB2_PROJETO1.py
@@ -2,3 +2,16 @@ def cumprimento(texto):
     return f"Olá, {texto}"
 nome_do_aluno = input("Digite seu nome:")
 print(cumprimento(nome_do_aluno))
+import random
+def media_dos_numeros():
+    numeros = [random.randint(0, 100) for _ in range(3)]
+
+    print("Números aleatórios sorteados:", numeros)
+
+    media = sum(numeros) / len(numeros)
+
+    return media
+
+
+media = media_dos_numeros()
+print("A média dos três números aleatórios é:", media)
